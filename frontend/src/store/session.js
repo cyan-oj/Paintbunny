694652d2999822c284b02e7cc3b003df9f309fdc
cyan <97122638+cyan-oj@users.noreply.github.com>
2022-10-09 20:14:19 -0400

    add fom page for login

---
+++ frontend/src/store/session.js
@@ -0,0 +1,45 @@
+import csrfFetch from "./csrf";
+
+const SET_CURRENT_USER = "session/setCurrentUser";
+const REMOVE_CURRENT_USER = "session/removeCurrentUser";
+
+const setCurrentUser = user => ({
+  type: SET_CURRENT_USER,
+  payload: user
+});
+
+const removeCurrentUser = () => ({
+  type: REMOVE_CURRENT_USER
+});
+
+const storeCurrentUser = user => {
+  if (user) sessionStorage.setItem("currentUser", JSON.stringify(user));
+  else sessionStorage.removeItem("currentUser");
+}
+
+export const login = ({ credential, password }) => async dispatch => {
+  const response = await csrfFetch("/api/session", {
+    method: "POST",
+    body: JSON.stringify({ credential, password })
+  });
+  const data = await response.json();
+  storeCurrentUser(data.user);
+  dispatch(setCurrentUser(data.user));
+  return response;
+};
+
+
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return { ...state, user: action.payload };
+    case REMOVE_CURRENT_USER:
+      return { ...state, user: null };
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
