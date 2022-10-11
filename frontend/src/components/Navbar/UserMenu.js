3c1af58bfb09763eb858ac7b48d42ddac90d9c95
cyan <97122638+cyan-oj@users.noreply.github.com>
2022-10-11 17:46:28 -0400

    remove some extraneous debuggers and console logs

--- frontend/src/components/Navbar/UserMenu.js
+++ frontend/src/components/Navbar/UserMenu.js
@@ -1,10 +1,10 @@
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
 import { logout } from "../../store/session"
 import { NavLink } from "react-router-dom";
 
 function UserMenu({userId}) {
   const dispatch = useDispatch();
- // const { userId } = useSelector( state => state.session.user.id)
+
   return (
     <div className="usermenu">
       <NavLink 
