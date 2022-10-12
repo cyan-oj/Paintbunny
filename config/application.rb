4100ba5331a315f84934b753c92a666f92977a3d
cyan <97122638+cyan-oj@users.noreply.github.com>
2022-10-12 14:56:43 -0400

    heroku configuration

--- config/application.rb
+++ config/application.rb
@@ -30,16 +30,17 @@ module Paintbunny
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.api_only = true
+
+    config.railties_order = [:all, :main_app]
 
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore,
       key: '_paintbunny_session',
       same_site: :lax, 
       secure: Rails.env.production?
-
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
   end
 end
