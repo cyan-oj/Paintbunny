3cd69cc92cf4b01212068009ddd8b5ad8f3070be
cyan <97122638+cyan-oj@users.noreply.github.com>
2022-10-14 11:06:08 -0400

    clean up workspace

--- frontend/src/components/Painter/WorkSpace.js
+++ frontend/src/components/Painter/WorkSpace.js
@@ -1,47 +1,13 @@
 import React, { useState } from "react"
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import csrfFetch from "../../store/csrf";
 import Canvas from "./Canvas"
 import './Painter.css'
 
 function WorkSpace() {
-  // const dispatch = useDispatch();
   const user = useSelector(state => state.session.user)
 
   const [title, setTitle] = useState('');
-  //const [drawingFile, setDrawingFile] = useState(null);
-
-  // const saveCanvas = () => {
-  //   const link = document.getElementById("link")
-  //   const canvas = document.getElementById("canvas")
-  //   link.setAttribute("download", "paintbunny.png");
-  //   link.setAttribute("href", canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
-  //   link.click();
-  // }
-
-  // const uploadFile = async e => {
-  //   e.preventDefault();
-  //   const formData = new FormData();
-  //   formData.append('drawing[title]', title);
-  //   formData.append('drawing[artist_id]', user.id)
-  //   formData.append('drawing[image]', drawingFile)
-
-  //   const response = await csrfFetch(`/api/users/${user.id}/drawings`, {
-  //     method: "POST",
-  //     body: formData
-  //   });
-  //   if (response.ok) {
-  //     const message = await response.json();
-  //     console.log("message", message.message);
-  //     setTitle('');
-  //     setDrawingFile(null);
-  //   }
-  // }
-
-  // const handleFile = e => {
-  //   const file = e.currentTarget.files[0];
-  //   setDrawingFile(file);
-  // }
 
   function dataURItoBlob(dataURI) { // todo: check gif libraries to optimize this process
     const binary = atob(dataURI.split(',')[1]);
@@ -77,7 +43,6 @@ function WorkSpace() {
   return (
     <>
       <Canvas id="canvas" width="300" height="300" style={{ backgroundColor: "white" }}/>
-      {/* <button onClick={ saveCanvas }>save canvas</button> */}
       <form onSubmit={ blobCanvas }>
         <input
           type="text"
@@ -85,10 +50,6 @@ function WorkSpace() {
           value={ title }
           onChange={ e => setTitle(e.target.value) }
         />
-        {/* <input 
-          type="file"
-          onChange={ handleFile }
-          /> */}
           <input 
             type="submit" 
             value="post drawing" 
