3de5f7b1b642472bcdde15e83e7e7d86daf2a76a
cyan <97122638+cyan-oj@users.noreply.github.com>
2022-10-15 14:49:19 -0400

    add context ref for canvas

--- frontend/src/components/Painter/Canvas.js
+++ frontend/src/components/Painter/Canvas.js
@@ -2,10 +2,32 @@ import React, { useEffect, useRef } from "react";
 
 const Canvas = props => {
   const canvasRef = useRef(null)
+  const contextRef = useRef(null)
 
-  const stroke = (context, e, color = "rgba(0,0,0)", size = 5 ) => {
-    if (e.buttons !== 1) return; 
+  const position = { 
+    x: 0, 
+    y: 0 
+  }
 
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    
+    context.fillStyle = "white";
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+
+    contextRef.current = context;
+  }, []);
+
+  const setPosition = e => {
+    const box = e.target.getBoundingClientRect();
+    position.x = e.clientX - box.left;
+    position.y = e.clientY - box.top;
+    console.log(position)
+  }
+
+  const draw = ( e, context, color = "rgba(0,0,0)", size = 5 ) => {
+    if (e.buttons !== 1) return;
     context.imageSmoothingEnabled = false;
     context.beginPath();
     
@@ -18,42 +40,19 @@ const Canvas = props => {
     context.lineTo(position.x, position.y);
     context.stroke();
   }
-  
-  const position = { 
-    x: 0, 
-    y: 0 
-  }
 
-  const setPosition = e => {
-    const box = e.target.getBoundingClientRect();
-    position.x = e.clientX - box.left;
-    position.y = e.clientY - box.top;
-  }
-
-  useEffect(() => {
-    const canvas = canvasRef.current
-    const context = canvas.getContext('2d')
-    
-    context.fillStyle = "white";
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-
-    const draw = e => {
-      if (e.buttons !== 1) return;
-      stroke(context, e)
-    }
-
-    canvas.addEventListener("mousemove", draw)
-    canvas.addEventListener("mousenter", setPosition)
-    canvas.addEventListener("mousedown", setPosition)
-
-    return () => {
-      canvas.removeEventListener("mousemove", draw)
-      canvas.removeEventListener("mousedown", setPosition)
-      canvas.removeEventListener("mousenter", setPosition)
-    }
-  }, []);
-
-  return <canvas ref={canvasRef} {...props} />
+  return (
+  <canvas 
+    ref={ canvasRef } 
+    {...props} 
+    onMouseDown={ setPosition }
+    onMouseEnter={ setPosition }
+    onMouseMove={ e => draw( 
+      e,
+      contextRef.current,
+      "red"
+    )}/>
+  )
 } 
 
 export default Canvas;
