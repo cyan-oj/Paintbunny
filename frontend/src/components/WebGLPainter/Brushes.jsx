f101bc9ced26610aba0a57b771c5049b2b826ad7
cyan <97122638+cyan-oj@users.noreply.github.com>
2023-03-21 13:15:55 -0400

    label brush menu inputs

--- frontend/src/components/WebGLPainter/Brushes.jsx
+++ frontend/src/components/WebGLPainter/Brushes.jsx
@@ -22,15 +22,27 @@ function Brushes({ brushes, activeBrush, brushThumbnails, paintDispatch, showBru
       { brushList }
       { showBrushTools &&
         <div className='sliders'>
-          <input type='range' min='0.01' step="0.01" max='10' value={ activeBrush.scale }
+          <input type='range' min='0.05' step="0.05" max='10' value={ activeBrush.scale }
             onChange={ e => paintDispatch({ type: 'brush_scale', payload: e.target.value })}
             />
+          <label htmlFor="number">size
+            <input type="number" min='0.05' step="0.05" max='10' value={ activeBrush.scale }
+              onChange={ e => paintDispatch({ type: 'brush_scale', payload: e.target.value })} />
+          </label>
           <input type='range' min='0' max='360' value={ activeBrush.angle }
             onChange={ e => paintDispatch({ type: 'brush_angle', payload: e.target.value })}
             />
+          <label htmlFor="number">angle
+            <input type="number" min='0' max='360' value={ activeBrush.angle }
+            onChange={ e => paintDispatch({ type: 'brush_angle', payload: e.target.value })} />
+          </label>
           <input type='range' min='0.01' step=".01" max="1" value={ activeBrush.ratio }
             onChange={ e => paintDispatch({ type: 'brush_ratio', payload: e.target.value })}
             />
+          <label htmlFor="number">ratio
+            <input type="number" min='0.01' step=".01" max="1" value={ activeBrush.ratio }
+            onChange={ e => paintDispatch({ type: 'brush_ratio', payload: e.target.value })} />
+          </label>
         </div>
       }
     </>
